33b8c07d5a8cbee177b551ec712444b14011ab5f
Nicolas <nicolaslechenic@gmail.com>
2014-12-16 00:31:56 +0100

    Edit config.rb

--- config.rb
+++ config.rb
@@ -3,32 +3,15 @@ $mhack_folder = '/Path/To/Mhack/Folder'
 
 #Ref
 $mhack_ref = $mhack_folder+'/ref'
-$mhack_ci = $mhack_ref+'/codeigniter'
 
 #App path
 $editor_app = '/Path/To/Editor.app'
-$pgadmin_app = '/Path/To/pgAdmin3.app'
-$sublime_app = '/Path/To/Sublime\ Text\ 2.app'
-
-$preview_app = "/Path/To/Preview.app"
 
 #Web path
-$chrome_app = '/Path/To/Google\ Chrome.app'
-$firefox_app = '/Path/To/Firefox.app'
+$browser_app = '/Path/To/Browser.app'
 
 #--Folders
 #Sites
-$sites_folder = '/Path/To/Sites/Folder'
 $rails_folder = '/Path/To/Ruby\ On\ Rails/Folder'
-$php_folder = '/Path/To/Php/Folder'
-$ci_folder = '/Path/To/Codeigniter/Folder'
-$js_folder = '/Path/To/Javascript/Folder'
 
-#Apps
-$apps_folder = '/Path/To/App/Folder'
-$ruby_folder = '/Path/To/Ruby/Folder'
-$py_folder = '/Path/To/Python/Folder'
-$sharp_folder = '/Path/To/C#/Folder'
 
-#Backup
-$backup_folder = '/Path/To/Backup/Folder'
