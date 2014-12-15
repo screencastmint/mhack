9c730575533003f711e83c6eab0e484834734965
Nicolas <nicolaslechenic@gmail.com>
2014-12-16 00:19:09 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require_relative 'config'
+require_relative 'mhack_modules'
 require_relative 'system/core'
 require_relative 'system/dispatcher'
+require 'rubygems'
+require 'rainbow'
+require 'fileutils'
+
 
 class Main
 	def main
