9efd482aacaf0e7c68bfe4b63b49a351405eb706
Nicolas <nicolaslechenic@gmail.com>
2014-12-16 19:27:05 +0100

    Release 0.0.5 Update dispacher

--- system/dispatcher.rb
+++ system/dispatcher.rb
@@ -1,35 +1,52 @@
 
-
 class Dispatcher
 
-
     def dispatcher
+        
+        if $techno
+            separator = $techno.split('-')
+            about = separator[0].downcase
+            $version = separator[1]
 
-    	separator = $techno.split('-')
-    	about = separator[0].downcase
-        $version = separator[1]
 
-    	case about
-        when "@all"
-            all = MhackmdAll::ALL.new()
-            all.cmd
-        when "@gem"
-            techno = MhackmdGem::GEM.new()
-            techno.cmd
-        when "@editor"
-            techno = MhackmdEditor::EDITOR.new()
-            techno.cmd
-        when "@rails"
-            techno = MhackmdRails::RAILS.new()
-            techno.cmd
-        when "@web"
-            techno = MhackmdWeb::WEB.new()
-            techno.cmd
-    	else
-    		 puts ""
-            puts Rainbow("/!\\ Unknown @techno !").background("#EA1B00") 
+            if about[0] != "@"
+                puts ""
+                puts Rainbow("/!\\ You have to puts technologie prefixed by @").background("#EA1B00") 
+                puts ""
+            else
+                
+                case about
+                when "@all"
+                    all = MhackmdAll::ALL.new()
+                    all.cmd
+                 when "@mac"
+                    techno = MhackmdMac::MAC.new()
+                    techno.cmd
+                when "@gem"
+                    techno = MhackmdGem::GEM.new()
+                    techno.cmd
+                when "@editor"
+                    techno = MhackmdEditor::EDITOR.new()
+                    techno.cmd
+                when "@rails"
+                    techno = MhackmdRails::RAILS.new()
+                    techno.cmd
+                when "@web"
+                    techno = MhackmdWeb::WEB.new()
+                    techno.cmd
+                else
+                    puts ""
+                    puts Rainbow("/!\\ Unknown @techno !").background("#EA1B00") 
+                    puts ""
+                end    
+
+            end            
+
+        else
             puts ""
-    	end
+            puts Rainbow("/!\\ You have to puts technologie prefixed by @").background("#EA1B00") 
+            puts ""
+        end
 
     end
 
