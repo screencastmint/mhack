e24789c1806747c2661d48b9a9dd2031a1040b41
Nicolas <nicolaslechenic@gmail.com>
2014-12-16 12:37:37 +0100

    Add @mac module

--- config.rb
+++ config.rb
@@ -4,11 +4,29 @@ $mhack_folder = '/Path/To/Mhack/Folder'
 #App path
 $editor_app = '/Path/To/Editor.app'
 
-#Web path
-$browser_app = '/Path/To/Browser.app'
 
 #--Folders
 #Sites
 $rails_folder = '/Path/To/Ruby\ On\ Rails/Folder'
+
+
+#--Folders
+#Mac
+$desktop_path = '/Path/To/Desktop'
+$download_folder = '/Path/To/Download/Folder'
+
+$images_folder = '/Path/To/Images/Folder'
+$music_folder = '/Path/To/Music/Folder'
+$movies_folder = '/Path/To/Movies/Folder'
+
+
+#Sites
+$sites_folder = '/Path/To/Local/Sites/Folder'
+$rails_folder = '/Path/To/Ruby\ On\ Rails/Folder'
+
+
+
+
+
 
 
--- mhack_modules.rb
+++ mhack_modules.rb
@@ -1,4 +1,12 @@
+#Main modules
+require 'mhackmd_mac'
 require 'mhackmd_gem'
 require 'mhackmd_editor'
 require 'mhackmd_rails'
 require 'mhackmd_web'
+
+#Official modules
+
+#Unofficial modules
+
+#Personnal modules
