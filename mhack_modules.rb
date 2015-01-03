e24bc69df11e95e450da246c12848fcef11237ef
Nicolas <nicolaslechenic@gmail.com>
2015-01-03 18:53:52 +0100

    Edit mhack_modules

--- mhack_modules.rb
+++ mhack_modules.rb
@@ -1,12 +1,13 @@
 #Main modules
 require 'mhackmd_brew'
+require 'mhackmd_finder'
 require 'mhackmd_mac'
 require 'mhackmd_gem'
 require 'mhackmd_editor'
 require 'mhackmd_web'
 
 #Official modules
-#require 'mhackmd_rails'
+#require 'mhackmd_desktop'
 
 
 #Unofficial modules
