662c652f136a87c074909ebf7e05f29afb5ef0fe
Nicolas <nicolaslechenic@gmail.com>
2015-01-07 00:26:28 +0100

    config.rb

--- config.rb
+++ config.rb
@@ -1,42 +1,15 @@
-#--Mhack
-$mhack_folder = '/Path/To/Mhack/Folder'
-
 #App path
 $editor_app = '/Path/To/Editor.app'
 
 
-#--Folders
-#Sites
-$rails_folder = '/Path/To/Ruby\ On\ Rails/Folder'
+#List of folders (Capitalize Listname into array)
+	#$path_list = ['Desktop', 'Images','Rails','...']
 
-
-#--Folders
-#Mac
-$desktop_path = '/Path/To/Desktop'
-$download_folder = '/Path/To/Download/Folder'
-
-$images_folder = '/Path/To/Images/Folder'
-$music_folder = '/Path/To/Music/Folder'
-$movies_folder = '/Path/To/Movies/Folder'
-
-
-#Sites
-$sites_folder = '/Path/To/Local/Sites/Folder'
-
-#$php_folder = '/Path/To/Php/Folder'
-#$ruby_folder = '/Path/To/Ruby/Folder'
-#$rails_folder = '/Path/To/Ruby\ On\ Rails/Folder'
-#$swift_folder = '/Path/To/Swift/Folder'
-#$oc_folder = '/Path/To/Objective-C/Folder'
-#$js_folder = '/Path/To/Javascript/Folder'
-#$c_folder = '/Path/To/C/Folder'
-#$cs_folder = '/Path/To/C\ Sharp/Folder'
-#$asp_folder = '/Path/To/Asp/Folder'
-#$cpp_folder = '/Path/To/C\ Plus\ Plus/Folder'
-#$django_folder = '/Path/To/Django/Folder'
-#$python_folder = '/Path/To/Python/Folder'
-#$java_folder = '/Path/To/Java/Folder'
-#$j2ee_folder = '/Path/To/J2EE/Folder'
+#Paths to folder
+	#$path_desktop = '/Path/To/Desktop'
+	#$path_images = '/Path/To/Images'
+	#$path_rails = '/Path/To/Rails'
+	#$path_... = '/Path/To/What/You/Want'
 
 
 #--Dock
