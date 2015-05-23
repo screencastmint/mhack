f5b5f54f272f1e7ddeaf1ec530199d1e2dd72be5
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-05-24 00:21:32 +0200

    Clean code

--- lib/mhack.rb
+++ lib/mhack.rb
@@ -1,10 +1,10 @@
 module Mhack
     class Dispatcher
         attr_reader :techno, :action, :params 
+
         def initialize
             @techno = ARGV[0]
             @action = ARGV[1]
-
             reader = ARGV.length
             @params = Array.new
 
@@ -17,48 +17,40 @@ module Mhack
 
         #Instantiates mhackmd designated by @something
         def pointer
-            if @techno
-                #Show @aliases defined into config or do some stuff about aliases
-                if @techno == "@aliases"
-                    if @action == ":show"
-                        puts ""
-                        puts $techno_aliases_list
-                        puts ""
-                    else
-                        puts ""
-                        puts Rainbow("/!\\ Unknow :action").background("#EA1B00") 
-                        puts ""
-                    end
+            if @techno && @techno == "@aliases"
+                if @action == ":show"
+                    render_message($techno_aliases_list)
                 else
-                    #Check if @techno is set
-                    if @techno[0] != "@"
-                        puts ""
-                        puts Rainbow("/!\\ You have to puts technologie prefixed by @").background("#EA1B00") 
-                        puts ""
-                    else
-                        param_count = @techno.length - 1
-                        param_name = @techno[1,param_count]
-                        get_techno = '$techno_'+param_name
-                        launch_techno = eval(get_techno)
-
-                        # Check aliases defined into config.rb
-                        if launch_techno
-                            techno = launch_techno.capitalize
-                        else
-                            techno = param_name.capitalize
-                        end
-
-                        #Launch Mhackmd
-                        concat = 'Mhackmd'+techno+'::'+techno+'.new'
-                        t = eval(concat)
-                        t.launcher  
-                    end 
+                    action_error
                 end
+            elsif @techno && @techno[0] == "@"
+                param_count = @techno.length - 1
+                param_name = @techno[1,param_count]
+                get_techno = '$techno_'+param_name
+                launch_techno = eval(get_techno)
+
+                techno = launch_techno ? (launch_techno.capitalize) : (param_name.capitalize)
+
+                instantiate = 'Mhackmd'+techno+'::'+techno+'.new'
+                eval(instantiate).launcher
             else
-                puts ""
-                puts Rainbow("/!\\ You have to puts technologie prefixed by @").background("#EA1B00") 
-                puts ""
-            end 
+                techno_error
+            end
+        end
+
+        private
+        def render_message(message)
+            puts ""
+            puts message
+            puts ""
+        end
+
+        def techno_error
+            render_message(Rainbow("/!\\ You have to puts technologie prefixed by @").background("#EA1B00"))
+        end
+
+        def action_error
+            render_message(Rainbow("/!\\ Unknow :action").background("#EA1B00"))
         end
     end
 end
