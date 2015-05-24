61eca7a3029ddde373a9c783388e64e3c5b83255
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-05-24 20:39:51 +0200

    add render messages

--- lib/helper.rb
+++ lib/helper.rb
@@ -10,15 +10,31 @@ class Helper
     render_line
   end
 
+  def render_success_message(message)
+    render_message(Rainbow(message).background("#046831"))
+  end
+
+  def render_question_message(message)
+    render_message(Rainbow(message).background("#CE4900"))
+  end
+
+  def render_inform_message(message)
+    render_message(Rainbow(message).background("#065AB9"))
+  end
+
+  def render_alert_message(message)
+    render_message(Rainbow(message).background("#EA1B00"))
+  end
+
   def render_techno_error
-      render_message(Rainbow("/!\\ You have to puts technologie prefixed by @").background("#EA1B00"))
+      render_alert_message("/!\\ You have to puts technologie prefixed by @")
   end
 
   def render_action_error
-      render_message(Rainbow("/!\\ Unknow :action").background("#EA1B00"))
+      render_alert_message("/!\\ Unknow :action")
   end
 
   def render_keyword_error
-    render_message(Rainbow("/!\\ Unknown Keyword").background("#EA1B00"))
+    render_alert_message("/!\\ Unknown Keyword")
   end
 end
