f2326d7cdaca909e04534f8184657691466c56b7
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-05-24 13:18:32 +0200

    edit render_line

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,7 +1,7 @@
 class Helper
 
   def render_line
-    puts "-"*50
+    puts "~"*80
   end
   
   def render_message(message)
@@ -17,5 +17,4 @@ class Helper
   def render_action_error
       render_message(Rainbow("/!\\ Unknow :action").background("#EA1B00"))
   end
-  
 end
