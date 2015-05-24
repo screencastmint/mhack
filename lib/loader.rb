fd7eb64163ed291ac3bbae29474ba31ddc4105f3
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-05-24 13:08:35 +0200

    router to hack

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,7 +1,7 @@
 require_relative '../config'
 require_relative '../mhack_modules'
 require_relative 'helper'
-require_relative 'router'
+require_relative 'mhack'
 require 'rubygems'
 require 'rainbow'
 require 'fileutils'
