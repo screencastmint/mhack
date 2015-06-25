7faee991d2ce75a012898b6844fa4ce7103449cb
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2015-06-25 08:47:23 +0200

    Add active_support

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,7 +1,9 @@
+require 'rubygems'
+require 'rainbow'
+require 'fileutils'
+require 'active_support/all'
+
 require_relative '../config'
 require_relative '../mhack_modules'
 require_relative 'helper'
 require_relative 'mhack'
-require 'rubygems'
-require 'rainbow'
-require 'fileutils'
